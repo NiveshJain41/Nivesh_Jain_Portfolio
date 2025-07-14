88b605d7113b4cd14bd5cdce1c7b5c4f83f23c0e
NiveshJain41 <niveshjain41@gmail.com>
2025-07-14 05:48:44 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Nivesh_Jain_Portfolio/", // ✅ move this here
   server: {
     host: "::",
     port: 8080,
